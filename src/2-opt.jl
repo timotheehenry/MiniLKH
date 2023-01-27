a701c12d2ab895a29bc7b01d32a9dbfea360753a
Timothée Henry <timothee.henry@gmail.com>
2023-01-27 10:48:03 +0100

    Update 2-opt.jl

--- src/2-opt.jl
+++ src/2-opt.jl
@@ -20,8 +20,11 @@ function Reverse_Segment(tour::Vector{Int};
         # swap(tour[left], tour[right])
         tour[left], tour[right] = tour[right], tour[left]
 
-        left  = (left + 1) % N
-        right = (N + right - 1) % N
+        #left  = (left + 1) % N
+        left = shift_1(N, left)
+        
+        #right = (N + right - 1) % N
+        right = shift_n(N, N-1, right)
 
     end
 
@@ -53,7 +56,7 @@ function Make_2_Opt_Move(tour; i, j)
     #   L1': from t[i] to t[j]   and  L2': from t[i+1] to t[j+1]
     # This is equivalent to reversing order in segment from 'i+1' to 'j'
     N = length(tour)
-    Reverse_Segment(tour, startIndex=(i+1) % N, endIndex=j)
+    Reverse_Segment(tour, startIndex=shift_1(N,i), endIndex=j)
 end
 
 
@@ -91,12 +94,12 @@ function LS_2_Opt(tour, distmat)
                 Y1 = tour[j]
                 Y2 = tour[shift_1(N, j)]
 
-                gain = Gain_From_2_Opt(distmat, X1, X2, Y1, Y2)
+                gainExpected = Gain_From_2_Opt(distmat, X1, X2, Y1, Y2)
 
-                if gain > 0
+                if gainExpected > bestMove.gain
                     # this move will shorten the tour, apply it at once
                     Make_2_Opt_Move(tour, i=i, j=j)
-                    print("Tour shortened by $gain \n")
+                    print("Tour shortened by $gainExpected \n")
                     locallyOptimal = false
                     break
                 end
@@ -105,3 +108,72 @@ function LS_2_Opt(tour, distmat)
     end
 
 end # end of LS_2_Opt
+
+
+
+
+# ============================
+# 2-opt
+# http://tsp-basics.blogspot.com/2017/03/2-opt-basic-iterative-algorithm.html
+# ============================
+mutable struct Move2
+    i::Int;
+    j::Int;
+    gain::Int;
+end
+
+function LS_2_Opt_Take_Best(tour, distmat)
+    # Shortens the tour by repeating 2-opt moves until no improvement
+    # can by done; in every iteration looks for and applies the move
+    # that gives maximal length gain.
+
+    N = size(distmat)[1]
+    locallyOptimal = false
+    bestMove = Move2(0,0,0)
+
+    while !(locallyOptimal)
+        locallyOptimal = true
+        bestMove.gain = 0
+
+        for counter_1 in 1:(N-2)
+            i = counter_1
+            #print("i = $i \n")
+
+            X1 = tour[i]
+            X2 = tour[shift_1(N,i)]
+
+            if i == 1
+                counter_2_Limit = N-1
+            else
+                counter_2_Limit = N
+            end
+
+            for counter_2 in (i+2):counter_2_Limit
+                j = counter_2
+                #print("j = $j \n")
+
+                Y1 = tour[j]
+                Y2 = tour[shift_1(N,j)]
+
+                gainExpected = Gain_From_2_Opt(distmat, X1, X2, Y1, Y2)
+
+                if gainExpected > bestMove.gain
+                    #print("improvement found \n")
+                    #print(bestMove, "\n")
+                    bestMove = Move2(i,
+                                    j,
+                                    gainExpected)
+                    #print(bestMove, "\n")
+
+                    locallyOptimal = false
+                end
+            end
+        end # end_for counter_1 loop
+
+        if !(locallyOptimal)
+            Make_2_Opt_Move(tour, i = bestMove.i, j = bestMove.j)
+            gain = bestMove.gain
+            print("Tour shortened by $gain \n")
+        end 
+    end # end_while not locallyOptimal 
+end 
